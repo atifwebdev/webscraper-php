f18274931b296747e9b57417669e99421f21a5ee
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2024-05-07 12:40:23 +0500

    scraper with pagination

--- goutte_requests.php
+++ goutte_requests.php
@@ -1,28 +1,27 @@
 <?php
-# scraping books to scrape: https://books.toscrape.com/
-# This is simple scrapper
+# scraping books to scrape: https://ocsheriff.gov/news/
+# Scrapper with pagination
+# Scrapping data saved in CSV format
 
 require 'vendor/autoload.php';
 $client = new \Goutte\Client();
 
-$url = 'https://books.toscrape.com/';
-
-while (true)
-{
+$postCount = 0;
+while (true) {
+    $url = 'https://ocsheriff.gov/news?page=' . $postCount;
     $crawler = $client->request('GET', $url);
 
+    $file = fopen("books.csv", "a"); //print data in CSV file format
     $crawler
-        ->filter('.row li article h3 a')
-        ->each(function ($node) use ($client) {
+        ->filter('.results div.views-row a.card-bordered-link div.card-bordered div.card-bordered__content h2 span')
+        ->each(function ($node) use ($file) {
             $name = $node->text();
+            // $scraped_data[] = [$node->text()];
             echo "{$name} \n";
+            fputcsv($file, [$name]);
         });
-
-    // Try to find the "Next" link
-    $next = $crawler->filter('li.next a');
-
-    // Stop, if there is no more "Next" link
-    if ($next->count() == 0) break;
-
-    $url = $next->link()->getUri();
+        fclose($file);
+    if ($postCount == 3) break; //put number of pages here to scrape
+    $postCount++;
+    echo "{$postCount} \n";
 }
